bf20fcd6d4c7c87a6d7adf68555598c688dd2d3b
Vityazzz5830 <121909224+Vityazzz5830@users.noreply.github.com>
2023-10-04 20:06:01 +0300

    Add files via upload

---
+++ js/modal.js
@@ -0,0 +1,42 @@
+window.addEventListener('DOMContentLoaded', function () {
+   MicroModal.init({
+      openTrigger: 'data-custom-open',
+      closeTrigger: 'data-micromodal-close',
+      disableScroll: true,
+      disableFocus: true,
+      awaitOpenAnimation: true,
+      awaitCloseAnimation: true,
+   });
+});
+
+Fancybox.bind("[data-fancybox]", {
+  // Your custom options
+});
+
+var ordercall_btn1 = document.getElementsByClassName("form_contacts-sendbtn-item-modal")[0];
+console.log(ordercall_btn1);
+
+//Кнопки перехода
+ordercall_btn1.addEventListener("click", function () {
+  event.preventDefault(); 
+
+    MicroModal.show('modal-consult');
+    MicroModal.close('modal-сatalog_item');
+});
+
+const btnclose = document.querySelector("#modal_closebtn");
+var body1 = document.getElementsByTagName('body')[0]
+
+btnclose.addEventListener("click", function () {
+  event.preventDefault(); 
+    MicroModal.close('modal-consult');
+    body1.setAttribute('style', 'overflow-y: scroll');
+});
+
+/*
+const over = document.getElementsByClassName("modal__overlay")[0];
+
+over.addEventListener("click", function () {
+  event.preventDefault(); 
+    MicroModal.close('modal-consult');
+});*/
